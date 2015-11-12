5c4d9815dedad9cce102a8d53a8205937510ec55
Philip Masser <p.masser@asim.at>
2015-11-12 17:16:38 +0100

    Links don't have scenes...

--- gg_core/model/gglink.h
+++ gg_core/model/gglink.h
@@ -11,18 +11,15 @@ class GG_CORESHARED_EXPORT GGLink
 public:
     GGLink();
 
-    QString sceneName() const { return m_sceneName; }
     QString name() const { return m_name; }
     GGConnection *connection() const { return m_connection; }
 
-    void setSceneName(QString sn) { m_sceneName = sn; }
     void setName(QString n) { m_name = n; }
     void setConnection(GGConnection *conn) { m_connection = conn; }
 
     // TODO: Add action
 
 protected:
-    QString m_sceneName;
     QString m_name;
     GGConnection *m_connection;
 
