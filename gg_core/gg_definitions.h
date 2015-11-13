f4bd044bb13abf8dab9ea9d8fe49098f955a20f7
Philip Masser <p.masser@asim.at>
2015-11-13 17:11:22 +0100

    Declare metatype for GG::PageID

--- gg_core/gg_definitions.h
+++ gg_core/gg_definitions.h
@@ -2,6 +2,7 @@
 #define GG_DEFINITIONS_H
 
 #include <QtGlobal>
+#include <QMetaType>
 
 namespace GG {
 typedef quint32 PageID;
@@ -10,6 +11,9 @@ typedef quint32 ConnectionID;
 static const PageID InvalidPageId = 0xFFFFFFFF;
 static const ConnectionID InvalidConnectionId = 0xFFFFFFFF;
 }
+
+Q_DECLARE_METATYPE(GG::PageID)
+//Q_DECLARE_METATYPE(GG::ConnectionID)
 
 #endif // GG_DEFINITIONS_H
 
