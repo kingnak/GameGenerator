b245ccecdf92e8ceca7a3f398d8cc658d0807371
Philip Masser <p.masser@asim.at>
2015-11-16 11:48:07 +0100

    Added tests

--- GGEditor_test/tst_ggeditor_viewcommandtest.cpp
+++ GGEditor_test/tst_ggeditor_viewcommandtest.cpp
@@ -1,10 +1,14 @@
 #include "tst_ggeditor_viewcommandtest.h"
 #include <viewcommand/ggviewcommands.h>
 #include <viewcommand/ggviewcommandfactory.h>
+#include <model/ggpage.h>
+#include <model/ggconnection.h>
 #include <model/ggeditmodel.h>
 #include <model/ggsimplefactory.h>
 #include <viewmodel/ggviewmodel.h>
 #include <viewmodel/ggviewpage.h>
+#include <viewmodel/ggviewconnection.h>
+#include <command/ggcommandstack.h>
 #include "testsignalchecker.h"
 
 GGEditor_ViewCommandTest::GGEditor_ViewCommandTest(QObject *parent) : QObject(parent)
@@ -12,39 +16,80 @@ GGEditor_ViewCommandTest::GGEditor_ViewCommandTest(QObject *parent) : QObject(pa
 
 }
 
+void GGEditor_ViewCommandTest::init()
+{
+    m_em = new GGEditModel(new GGSimpleFactory);
+    m_vm = new GGViewModel(m_em);
+    m_vf = new GGViewCommandFactory(m_vm);
+    m_vmsc = new ViewModelSignalChecker(m_vm);
+    m_stk = new GGCommandStack;
+}
+
+void GGEditor_ViewCommandTest::cleanup()
+{
+    delete m_stk;
+    delete m_vmsc;
+    delete m_vf;
+    delete m_vm;
+    delete m_em;
+}
+
 void GGEditor_ViewCommandTest::testCreateViewPage()
 {
-    GGEditModel em(new GGSimpleFactory);
-    GGViewModel vm(&em);
-    GGViewCommandFactory vf(&vm);
-    GGCreateViewPageCmd *cmd = vf.createStartPage(QRect(0, 0, 1 ,1));
-    ViewModelSignalChecker vmsc(&vm);
+
+    GGCreateViewPageCmd *cmd = m_vf->createStartPage(QRect(0, 0, 1 ,1));
 
     QVERIFY2(cmd->execute(), "Cannot execute create view page");
-    VERIFYVSIG(&vmsc, "Create View Page CMD", 1, 0, 0, 0, 0, 1);
+    VERIFYVSIG(m_vmsc, "Create View Page CMD", 1, 0, 0, 0, 0, 1);
     GGPage *p = cmd->getInnerCommand()->createdPage();
-    QVERIFY2(em.getPages().size() == 1, "No page created");
-    QVERIFY2(em.getPage(p->id()) == p, "Create page is not the page from the command");
-    QVERIFY2(vm.getViewPageForPage(p) != NULL, "No View Page Created");
-    QVERIFY2(vm.getViewPageForPage(p)->page() == p, "View Page not associated with page");
-    GGViewPage *vp = vm.getViewPageForPage(p);
+    QVERIFY2(m_em->getPages().size() == 1, "No page created");
+    QVERIFY2(m_em->getPage(p->id()) == p, "Create page is not the page from the command");
+    QVERIFY2(m_vm->getViewPageForPage(p) != NULL, "No View Page Created");
+    QVERIFY2(m_vm->getViewPageForPage(p)->page() == p, "View Page not associated with page");
+    GGViewPage *vp = m_vm->getViewPageForPage(p);
     QVERIFY2(!vp->bounds().isEmpty(), "Bounds not set");
 
     QVERIFY2(cmd->undo(), "Cannot undo create view page");
-    VERIFYVSIG(&vmsc, "Undo Create View Page CMD", 0, 1, 0, 0, 0, 0);
-    QVERIFY2(em.getPages().isEmpty(), "Page still there after undo create view page");
+    VERIFYVSIG(m_vmsc, "Undo Create View Page CMD", 0, 1, 0, 0, 0, 0);
+    QVERIFY2(m_em->getPages().isEmpty(), "Page still there after undo create view page");
     //QVERIFY2(vm.getViewPageForPage(p) == vp, "View Page lost before processing commands after undo create view page");
     //qApp->processEvents();
-    QVERIFY2(vm.getViewPageForPage(p) == NULL, "View Page still there after undo create view page");
+    QVERIFY2(m_vm->getViewPageForPage(p) == NULL, "View Page still there after undo create view page");
 
     QVERIFY2(cmd->redo(), "Cannot redo create view page");
-    VERIFYVSIG(&vmsc, "Redo Create View Page CMD", 1, 0, 0, 0, 0, 0);
+    VERIFYVSIG(m_vmsc, "Redo Create View Page CMD", 1, 0, 0, 0, 0, 0);
     //QVERIFY2(vm.getViewPageForPage(p) == NULL, "View Page already there before processing commands after redo create view page");
     //qApp->processEvents();
-    QVERIFY2(vm.getViewPageForPage(p) != NULL, "View Page lost after processing commands after redo create view page");
-    QVERIFY2(vm.getViewPageForPage(p) == vp, "View Page changed (not recycled) after redoing create view page");
-    VERIFYVSIGNULL(&vmsc, "After Create");
+    QVERIFY2(m_vm->getViewPageForPage(p) != NULL, "View Page lost after processing commands after redo create view page");
+    QVERIFY2(m_vm->getViewPageForPage(p) == vp, "View Page changed (not recycled) after redoing create view page");
+    VERIFYVSIGNULL(m_vmsc, "After Create");
 
     delete cmd;
+}
+
+void GGEditor_ViewCommandTest::testCreateViewConnection()
+{
+    QVERIFY(m_stk->execute(m_vf->createStartPage(QRect(0,0,1,1))));
+    QVERIFY(m_stk->execute(m_vf->createEndPage(QRect(10,10,1,1))));
+    VERIFYVSIG(m_vmsc, "Create pages", 2, 0, 0, 0, 0, 2);
+
+    GGViewPage *s = m_vm->getViewPageForPage(m_em->getPages()[0]);
+    GGViewPage *e = m_vm->getViewPageForPage(m_em->getPages()[1]);
+    QVERIFY2(m_stk->execute(m_vf->createConnection(s, e, GGConnectionSlot::StartConnection)), "Cannot create view connection");
+    VERIFYVSIG(m_vmsc, "Create Connection", 0, 0, 1, 0, 1, 0);
+    QVERIFY2(m_em->getConnections().size() == 1, "Connection not set in model");
+    GGConnection *c = m_em->getConnections()[0];
+    QVERIFY2(GG::as<GGStartPage>(s->page())->startConnection() == c, "Connection not set in page");
+    QVERIFY2(m_vm->getViewConectionForConnection(c) != NULL, "Cannot get ViewConnection for Connection");
+    GGViewConnection *vc = m_vm->getViewConectionForConnection(c);
+    QVERIFY2(vc->connection() == c, "Connection not set in ViewConnection");
+
+    QVERIFY2(m_stk->undo(), "Cannot undo create connection");
+    VERIFYVSIG(m_vmsc, "Undo Create View Connection", 0, 0, 0, 1, 1, 0);
+    QVERIFY2(m_em->getConnections().isEmpty(), "Connection not removed");
+    QVERIFY2(m_vm->getViewConectionForConnection(c) == NULL, "Can still get viewconnection for Connetion after undo");
+
+    QVERIFY2(m_stk->redo(), "Cannot redo create connection");
+    VERIFYVSIG(m_vmsc, "Redo Create Connection", 0, 0, 1, 0, 1, 0);
 }
 
--- GGEditor_test/tst_ggeditor_viewcommandtest.h
+++ GGEditor_test/tst_ggeditor_viewcommandtest.h
@@ -3,6 +3,12 @@
 
 #include <QtTest>
 
+class GGEditModel;
+class GGViewModel;
+class GGViewCommandFactory;
+class GGCommandStack;
+class ViewModelSignalChecker;
+
 class GGEditor_ViewCommandTest : public QObject
 {
     Q_OBJECT
@@ -10,7 +16,17 @@ public:
     explicit GGEditor_ViewCommandTest(QObject *parent = 0);
 
 private slots:
+    void init();
+    void cleanup();
     void testCreateViewPage();
+    void testCreateViewConnection();
+
+private:
+    GGEditModel *m_em;
+    GGViewModel *m_vm;
+    GGViewCommandFactory *m_vf;
+    GGCommandStack *m_stk;
+    ViewModelSignalChecker *m_vmsc;
 };
 
 #endif // TST_GGEDITOR_VIEWCOMMANDTEST_H
