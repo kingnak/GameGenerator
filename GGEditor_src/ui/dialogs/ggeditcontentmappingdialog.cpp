bf2189162529f9563bdace01bf33380eb054c71b
Philip Masser <p.masser@asim.at>
2016-03-08 18:12:06 +0100

    Dialog around mapping finished

--- GGEditor_src/ui/dialogs/ggeditcontentmappingdialog.cpp
+++ GGEditor_src/ui/dialogs/ggeditcontentmappingdialog.cpp
@@ -1,11 +1,14 @@
 #include "ggeditcontentmappingdialog.h"
 #include "ui_ggeditcontentmappingdialog.h"
+#include <QAction>
+#include <model/ggeditmodel.h>
 #include <model/ggpage.h>
 #include <model/gglink.h>
 #include <model/ggcontentelement.h>
 #include <view/ggmappingscene.h>
+#include <view/ggmappinguicontroller.h>
 
-GGEditContentMappingDialog::GGEditContentMappingDialog(QWidget *parent) :
+GGEditContentMappingDialog::GGEditContentMappingDialog(GGEditModel *model, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::GGEditContentMappingDialog),
     m_page(NULL)
@@ -13,6 +16,20 @@ GGEditContentMappingDialog::GGEditContentMappingDialog(QWidget *parent) :
     ui->setupUi(this);
     m_scene = new GGMappingScene(this);
     ui->graphicsView->setScene(m_scene);
+    m_ctrl = new GGMappingUIController(model, this);
+
+    ui->actionUndo->setShortcut(QKeySequence::Undo);
+    ui->actionRedo->setShortcut(QKeySequence::Redo);
+
+    this->addAction(ui->actionUndo);
+    this->addAction(ui->actionRedo);
+
+    connect(ui->actionUndo, SIGNAL(triggered(bool)), m_ctrl, SLOT(undo()));
+    connect(ui->actionRedo, SIGNAL(triggered(bool)), m_ctrl, SLOT(redo()));
+
+    connect(model, SIGNAL(pageUpdated(GGPage*)), this, SLOT(updatePage(GGPage*)));
+    connect(m_scene, SIGNAL(addedItem(QRect)), this, SLOT(addLink(QRect)));
+    connect(m_scene, SIGNAL(movedItem(int,QRect)), this, SLOT(moveLink(int,QRect)));
 }
 
 GGEditContentMappingDialog::~GGEditContentMappingDialog()
@@ -23,8 +40,41 @@ GGEditContentMappingDialog::~GGEditContentMappingDialog()
 void GGEditContentMappingDialog::setMappedPage(GGMappedContentPage *page)
 {
     m_page = page;
-    if (page && page->content()) {
-        QPixmap p = page->content()->preview(QSize());
-        m_scene->setMappedElement(p);
+    updatePage(page);
+}
+
+GGCommandStack *GGEditContentMappingDialog::getExecutedCommands()
+{
+    return m_ctrl->getCommandStack();
+}
+
+void GGEditContentMappingDialog::reject()
+{
+    m_ctrl->undoAll();
+
+    QDialog::reject();
+}
+
+void GGEditContentMappingDialog::addLink(QRect rect)
+{
+    m_ctrl->createLink(m_page, rect);
+}
+
+void GGEditContentMappingDialog::moveLink(int idx, QRect rect)
+{
+    m_ctrl->moveLink(m_page, idx, rect);
+}
+
+void GGEditContentMappingDialog::updatePage(GGPage *page)
+{
+    if (page && page == m_page) {
+        if (m_page->content()) {
+            QPixmap p = m_page->content()->preview(QSize());
+            m_scene->setMappedElement(p);
+        }
+
+        QList<GGConnectionSlot> slts = GGConnectionSlot::enumerateConnections(m_page, GGConnectionSlot::MappedConnection);
+        ui->wgtLinks->setConnections(m_page, slts);
+        m_scene->setConnections(m_page, slts);
     }
 }
--- GGEditor_src/ui/dialogs/ggeditcontentmappingdialog.h
+++ GGEditor_src/ui/dialogs/ggeditcontentmappingdialog.h
@@ -3,8 +3,12 @@
 
 #include <QDialog>
 
+class GGEditModel;
+class GGPage;
 class GGMappedContentPage;
 class GGMappingScene;
+class GGMappingUIController;
+class GGCommandStack;
 
 namespace Ui {
 class GGEditContentMappingDialog;
@@ -15,15 +19,26 @@ class GGEditContentMappingDialog : public QDialog
     Q_OBJECT
 
 public:
-    explicit GGEditContentMappingDialog(QWidget *parent = 0);
+    explicit GGEditContentMappingDialog(GGEditModel *model, QWidget *parent = 0);
     ~GGEditContentMappingDialog();
 
     void setMappedPage(GGMappedContentPage *page);
+
+    GGCommandStack *getExecutedCommands();
+
+public slots:
+    void reject();
+
+private slots:
+    void addLink(QRect rect);
+    void moveLink(int idx, QRect rect);
+    void updatePage(GGPage *page);
 
 private:
     Ui::GGEditContentMappingDialog *ui;
     GGMappedContentPage *m_page;
     GGMappingScene *m_scene;
+    GGMappingUIController *m_ctrl;
 };
 
 #endif // GGEDITCONTENTMAPPINGDIALOG_H
